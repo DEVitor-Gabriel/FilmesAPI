8ffa726e0c927e8f63a6f9a741e1d02f5e41f2e1
Vitor Gabriel <vitor.lima@conveste.combr>
2023-02-07 14:08:14 -0300

    Realizando update completo com put

--- FilmesAPI/Controllers/FilmesController.cs
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -51,4 +51,17 @@ public class FilmesController : ControllerBase
         if (filme == null) return NotFound();
         return Ok(filme);
     }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        Filme? filme = _context.Filmes.FirstOrDefault(
+            filme => filme.Id == id
+            );
+
+        if (filme == null) return NotFound();
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
 }
---
+++ FilmesAPI/Data/DTO/UpdateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTO
+{
+    public class UpdateFilmeDto
+    {
+        [Required(ErrorMessage = "O titulo é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O genero é obrigatorio")]
+        [StringLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "A duração é obrigatorio")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
